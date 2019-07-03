b5255d7fe1561ef0a894c19747babff1012a9dbe
zangyuechao <markcoder@outlook.com>
2019-07-03 16:40:17 +0800

    小程序项目

---
+++ components/like/index.js
@@ -0,0 +1,40 @@
+// components/like/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    like: {
+      type: Boolean,
+      value: false
+    },
+    count: {
+      type: Number,
+      value: 0
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    yesSrc: './images/like.png',
+    noSrc: './images/dislike.png'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onLike (event) {
+      let like = this.properties.like
+      let count = this.properties.count
+
+      count = like ? (count - 1) : (count + 1)
+      this.setData({
+        count,
+        like: !like
+      })
+    }
+  }
+})
